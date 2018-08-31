dd09bdbefbc49e6d1ba7655e6a01f6d8b99fc31e
Stephen Powell <stephenmpowell4@gmail.com>
2018-08-31 16:04:05 -0400

    function names and sanitize function moved to seperate file

--- js/code.js
+++ js/code.js
@@ -1,7 +1,31 @@
-// testAlphaNumeric takes in a string and tests it for any char that is not alphanumeric or a underscore
-// if a non-alphanumeric char is found the function will return false else true
-function isAlphaNumeric(str)
+// TODO: Global variables
+
+function doLogin()
 {
-	let regex = /\W/;	// regular expresion for any non alphanumeric or underscore character
-	return !(regex.test(str));
+	// TODO:
+}
+
+function doLogout()
+{
+	// TODO:
+}
+
+function doRegister()
+{
+	// TODO:
+}
+
+function addContact()
+{
+	// TODO:
+}
+
+function searchContact()
+{
+	// TODO:
+}
+
+function deleteContact()
+{
+	// TODO:
 }
