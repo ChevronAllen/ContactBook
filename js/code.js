50b6c00484a2a4d9041876c5058fdc797da16341
Stephen Powell <stephenmpowell4@gmail.com>
2018-08-31 14:14:13 -0400

    public use md5 hash and sanitize function rename

--- js/code.js
+++ js/code.js
@@ -1,6 +1,6 @@
 // testAlphaNumeric takes in a string and tests it for any char that is not alphanumeric or a underscore
 // if a non-alphanumeric char is found the function will return false else true
-function testAlphaNumeric(str)
+function isAlphaNumeric(str)
 {
 	let regex = /\W/;	// regular expresion for any non alphanumeric or underscore character
 	return !(regex.test(str));
