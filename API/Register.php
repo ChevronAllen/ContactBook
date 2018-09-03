aaa2af055012c66ce055dbf21ef7264165f3d848
Chevron Allen <chevronallen@gmail.com>
2018-09-03 17:33:44 -0400

    Registration.php
    simple implementation

--- API/Register.php
+++ API/Register.php
@@ -1,12 +1,76 @@
 <?php
+require("SQL_Credentials.php");
 
-/* TODO:
-  Read JSON input
-  Sanitize fields
-  Run stored procedure
-  Echo Response JSON
-*/
+//	Make Connection
+$conn = new mysqli($serverURL, $serverLogin, $serverAuth, $serverDB);
 
+//	Get JSON input
+$inData = getRequestInfo();
+
+$id = 0;
+$firstName 	= "";
+$lastName 	= "";
+$username 	= "";
+$password 	= "";
+
+
+//	TODO:	proper check for JSON POST data http://thisinterestsme.com/receiving-json-post-data-via-php/
+if($inData  == NULL)
+{
+	returnWithError("Communications Error, NULL input");
+	
+//	Test for connection errors
+}else if($conn->connect_error)
+{
+	returnWithError("Error Connecting to the Server");
+}else
+{
+	//	Sanitize JSON input
+	$username = $mysqli->real_escape_string($inData["username"]);
+	$firstName = $mysqli->real_escape_string($inData["firstName"];
+	$lastName = $mysqli->real_escape_string($inData["lastName"]);
+	$password = $mysqli->real_escape_string($inData["password"]);
+	
+	//	Call stored procedure that will insert a new user
+	$sql = 'CALL proc_Add_New_User('  . $firstName . ',
+									' . $lastName . ',
+									' . $username . ',
+									' . $password . ')';
+	//	Capture results
+	$results = conn->query($sql);
+	
+	/*
+		result should be a row from the users table
+		capture the new users id to be sent back
+		we recieve the whole row so that if we need to implement 
+		a session id that would be sent.
+	*/
+	if ($result->num_rows <= 0){
+		returnWithError("Error adding new user");
+	}else{
+		
+				
+		$row = $result->fetch_assoc();
+		//$firstName = $row["user_firstname"];
+		//$lastName = $row["user_lastname"];
+		$id = $row["iduser"];			
+		
+		
+		//	if the id is zero somethign went wrong
+		if($id == 0)
+		{
+			returnWithError("Error adding new user");
+		}else
+		{
+			returnWithInfo($id, $firstName, $lastName);
+		}			
+	}
+}
+
+// Close the connection
+$conn->close();
+
+//	Retrieves data sent to the php
 function getRequestInfo()
 {
   return json_decode(file_get_contents('php://input'), true);
@@ -38,7 +102,7 @@ function returnWithError( $err )
   sendResultInfoAsJson( $retValue );
 }
 
-function returnWithInfo( $firstName, $lastName, $id )
+function returnWithInfo($id, $firstName, $lastName )
 {
   $retValue = createJSONString($id,$firstName,$lastName,"","","","");
   sendResultInfoAsJson( $retValue );
