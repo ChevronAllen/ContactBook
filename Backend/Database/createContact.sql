b94d785ffeed68925291bffc6198a90b478fed71
Chevron Allen <chevronallen@gmail.com>
2018-09-04 22:20:12 -0400

    Correction again sorry

--- Backend/Database/createContact.sql
+++ Backend/Database/createContact.sql
@@ -1,7 +1,7 @@
 /*
- accepts parameters userFirstName, userLastName, username, password, sessionID
- outputs the created users row.
- creates a session by inserting the user's sessionID
+ accepts parameters userID, contactFirstName, contactLastName, contactAddress, 
+					contactCity, contactState, contactZipCode, sessionID
+ outputs the created contact row.
  doesnt run the function if userID is invalid,or sessionID is expired (30 mins)
  updates date_lastLogin
 */ 
