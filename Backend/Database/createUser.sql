6d0e5431b2fbf15a48c37bbb897b70a839751462
Unknown <ryan.dozier@knights.ucf.edu>
2018-09-06 10:10:20 -0400

    final_stored_procedures
    Few changes when migrating db to web-server.

--- Backend/Database/createUser.sql
+++ Backend/Database/createUser.sql
@@ -7,7 +7,7 @@
 CREATE DEFINER=`root`@`%` PROCEDURE `createUser`(IN u_fname VARCHAR(45), u_lname VARCHAR(45), uname VARCHAR(45), u_pass VARCHAR(45), sessionID VARCHAR(32))
 BEGIN
 
-INSERT INTO USER (username, user_firstname, user_lastname, user_password, session_id)
+INSERT INTO user (username, user_firstname, user_lastname, user_password, session_id)
 VALUES (uname, u_fname, u_lname, u_pass, sessionID);
 
 SELECT userid, username, user_firstname, user_lastname, date_added, date_last_login, sessionID
--- Backend/Database/deleteContact.sql
+++ Backend/Database/deleteContact.sql
@@ -6,21 +6,22 @@
  updates date_lastLogin
 */
 
-CREATE DEFINER=`root`@`%` PROCEDURE `findContact`(IN user_id INT, matchString VARCHAR(32), sessionID VARCHAR(32))
+CREATE DEFINER=`root`@`%` PROCEDURE `deleteContact`(IN user_id INT, contact_id INT, sessionID VARCHAR(32))
 BEGIN
-
 DECLARE last_login DATETIME;
 SET last_login = (SELECT date_last_login FROM user WHERE userid= user_id AND session_id= sessionID LIMIT 1);
 IF  last_login IS NOT NULL THEN
 	IF (timestampdiff(MINUTE, last_login, now()) < 30) THEN
-		IF matchString IS  NULL OR matchString = '' THEN
-			SELECT * FROM contact
-			WHERE  userid = user_id;
-		ELSE
-			SELECT * FROM contact
-			WHERE  userid = user_id
-			LIKE matchString;
-		END IF;
+		DELETE FROM contact WHERE contactid = contact_id;
+
+	UPDATE user
+	SET date_last_login = CURRENT_TIMESTAMP()
+    WHERE userid= user_id
+    LIMIT 1;
+
+    SELECT   userid, username, user_firstname, user_lastname, date_added, date_last_login, session_id
+    FROM user
+    WHERE userid= user_id;
 	END IF;
 END IF;
 END
