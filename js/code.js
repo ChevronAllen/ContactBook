b72dec8a3b571692ea7224dd6ba2ceaf823fd45b
Stephen Powell <stephenmpowell4@gmail.com>
2018-09-09 15:29:59 -0400

    syntax

--- js/code.js
+++ js/code.js
@@ -443,7 +443,7 @@ function deleteContact()
 	var contactID = contacts[currentSelected].contactID;
 	var jsonPayload = '{'
 			+ '"id":'					+ userId    + ','
-			+ '"ContactID":'	+ ContactID + ','
+			+ '"ContactID":'	+ contactID + ','
 			+ '"sessionID":"'	+ sessionID
 			+ '"}';
 
